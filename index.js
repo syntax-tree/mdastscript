0d99ee87c94edf28724f1718c860d4031078b82f
Yoshiya Hinosawa <stibium121@gmail.com>
2018-01-28 13:35:10 +0900

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+export const ROOT = 'root'
+export const PARAGRAPH = 'paragraph'
+export const BLOCKQUOTE = 'blockquote'
+export const HEADING = 'heading'
+export const CODE = 'code'
+export const INLINE_CODE = 'inline_code'
+// TODO: add rest of types
